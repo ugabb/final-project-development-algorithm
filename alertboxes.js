80cf3aae2e92883cdeaec51678083fdc9953bf38
Gabriel Barros <gsb.alternativo@gmail.com>
2022-10-17 21:13:03 -0300

    maior quase pronto

--- alertboxes.js
+++ alertboxes.js
@@ -1,6 +1,8 @@
 const btnImc = document.getElementById('btnImc');
-const alertBox = document.getElementById('alertbox');
+const btnMaior = document.getElementById('btnMaior');
+const alertBox = document.getElementById('alertboxContainer');
 
+//AlertBox para a função IMC
 btnImc.addEventListener('click', (e) => {
     e.preventDefault();
 
@@ -8,13 +10,56 @@ btnImc.addEventListener('click', (e) => {
     div.innerHTML += `
         <div id="alertbox" class="alertbox">
             <h1>Questão 1</h1>
-            <label for="">Digite o Peso</label>
+            <label for="">Digite o Peso(kg)</label>
             <input type="number" name="" id="peso">
             <label for="">Digite a Altura(cm)</label>
             <input type="number" name="" id="alt">
-            <button onclick="imc()">Calcular</button>
+            <button class="btn" id="btnRes" onclick="imc()">Calcular</button>
+            <div id="res"></div>
+            <button class="btn" id="voltar" onclick="voltar()">Voltar</button>
         </div>
 
     `;
     alertBox.appendChild(div);
 })
+
+
+btnMaior.addEventListener('click', e => {
+    e.preventDefault();
+
+    var div = document.createElement('div');
+    div.innerHTML += `
+        <div id="alertbox" class="alertbox">
+            <h1>Questão 2 - Maior Valor</h1>
+            <label for="">Digite 20 valores positivos</label>
+            <div class="inputContainer">
+                <input type="number" name="" id="valores">
+                <button id="addValor" class="add" onclick="addArray()">+</button>
+            </div>
+            <div id="list"></div>
+            <button class="btn" id="btnRes" onclick="maior()">Calcular</button>
+            <div id="res"></div>
+            <button class="btn" id="voltar" onclick="voltar()">Voltar</button>
+        </div>
+
+    `;
+    alertBox.appendChild(div);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// Botão para voltar
+const voltar = () => {
+    const res = document.getElementById('alertbox');
+
+    res.remove();
+}
--- questions.js
+++ questions.js
@@ -1,3 +1,6 @@
+
+
+
 const teste = () => {
     console.log('works')
 }
@@ -8,21 +11,82 @@ const imc = () => {
     // let alt = Number(prompt("Digite sua altura:"));
     const peso = Number(document.getElementById('peso').value);
     const alt = Number(document.getElementById('alt').value);
-    console.log(peso,alt)
+    console.log(peso, alt)
     const res = peso / (alt * alt);
     const formatado = (res * 10000).toFixed(1)
 
     //console.log(formatado)
+    const pRes = document.getElementById('res');
 
     if (formatado < 20.7) {
-        alert(`seu imc é ${formatado} // Você está abaixo do peso`);
+        const div = document.createElement('div');
+        div.innerHTML = `<p style="text-align:center;">Seu IMC é ${formatado} <br> Você está abaixo do peso</p>`;
+        pRes.appendChild(div);
+
     } else if (formatado >= 20.7 && formatado < 26.4) {
-         alert(`seu imc é ${formatado} // Você está no peso ideal`);
+        const div = document.createElement('div');
+        div.innerHTML = `<p style="text-align:center;">Seu IMC é ${formatado} <br> Você está no peso ideal</p>`;
+        pRes.appendChild(div);
     } else if (formatado >= 26.4 && formatado < 27.9) {
-        alert(`seu imc é ${formatado} // Você está um pouco acima do peso`);
+        const div = document.createElement('div');
+        div.innerHTML = `<p style="text-align:center;">Seu IMC é ${formatado} <br> Você está um pouco acima do peso</p>`;
+        pRes.appendChild(div);
     } else if (formatado >= 27.9 && formatado < 31.1) {
-        alert(`seu imc é ${formatado} // Você está acima do peso`);
+        const div = document.createElement('div');
+        div.innerHTML = `<p style="text-align:center;">Seu IMC é ${formatado} <br> Você está acima do peso</p>`;
+        pRes.appendChild(div);
     } else if (formatado >= 31.1) {
-        alert(`seu imc é ${formatado} // Você está acima da obesidade`);
+        const div = document.createElement('div');
+        div.innerHTML = `<p style="text-align:center;">Seu IMC é ${formatado} <br> Você está acima da obesidade</p>`;
+        pRes.appendChild(div);
     }
+
+
+
+
+
+    // const div = document.createElement('div');
+    // div.innerHTML = `<p></p>`
+
+
+
+}
+
+let array = [];
+
+const addArray = () => {
+    const valores = document.getElementById('valores').value;
+    const pRes = document.getElementById('list');
+    
+    if (valores >= 0) {
+        var valor = Number(valores);
+        if(array.length < 20){
+           array.push(valor);
+        }else{
+            alert('Vetor cheio');
+        }
+        
+        console.log(array)
+        // const div = document.createElement('p');
+        // div.innerHTML = `<p style="text-align:center;">${array}</p>`;
+        // pRes.appendChild(div);
+    } else {
+        alert('Digite valores maior que 0');
+    }
+}
+
+const maior = () => {
+    const pRes = document.getElementById('res');
+    var maior = array[0];
+    for (let i = 0; i <= array.length; i++) {
+
+        if (array[i] > maior) {
+            maior = array[i];
+        }
+    }
+    const div = document.createElement('div');
+    div.innerHTML = `<p style="text-align:center;">O maior valor é ${maior}</p>`;
+    pRes.appendChild(div);
+
+    return console.log(maior);
 }
